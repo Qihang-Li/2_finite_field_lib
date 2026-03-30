04dc7b7a6fcfc3e71a2378eda043791c28e99f17
hangpedia <hangpedia@gmail.com>
2026-03-30 21:49:16 +0800

    implement state and boundary initialization with TDD

--- src/lib.rs
+++ src/lib.rs
@@ -18,15 +18,21 @@ impl Galois {
 mod tests {
     use super::*;
 
+    #[test] // Galois::new(0, 3) = 0
+    fn construction_check_zero() {
+        let result = Galois::new(BigUint::from(0u64), BigUint::from(3u64));
+        assert_eq!(result.num, BigUint::ZERO);
+    }
+
     #[test] // Galois::new(3, 5) = 3
     fn construction_check_basic() {
         let result = Galois::new(BigUint::from(3u64), BigUint::from(5u64));
         assert_eq!(result.num, BigUint::from(3u64));
     }
 
-    #[test] // Galois::new(0, 7) = 0
-    fn construction_check_zero() {
-        let result = Galois::new(BigUint::from(0u64), BigUint::from(7u64));
+    #[test] // Galois::new(14， 7) = 0
+    fn construction_check_multiple() {
+        let result = Galois::new(BigUint::from(14u64), BigUint::from(7u64));
         assert_eq!(result.num, BigUint::ZERO);
     }
 
@@ -34,12 +40,6 @@ mod tests {
     fn construction_check_modulus() {
         let result = Galois::new(BigUint::from(13u64), BigUint::from(11u64));
         assert_eq!(result.num, BigUint::from(2u64));
-    }
-
-    #[test] // Galois::new(26, 13) = 0
-    fn construction_check_multiple() {
-        let result = Galois::new(BigUint::from(26u64), BigUint::from(13u64));
-        assert_eq!(result.num, BigUint::ZERO);
     }
 
     #[test] // 553059516537161321408265876841 is a verified prime.
