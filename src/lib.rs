1cb63b1906206a499d5fbc9d109189dbce256c27
hangpedia <hangpedia@gmail.com>
2026-03-31 20:15:37 +0800

    derive equivalence, formatting, and cloning traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use num_bigint::BigUint;
 
+#[derive(PartialEq, Eq, Debug, Clone)]
 pub struct Galois {
     pub num: BigUint,
     pub prime: BigUint,
@@ -55,5 +56,82 @@ mod tests {
     #[should_panic] // We don't want the prime to be 0.
     fn construction_zero_prime() {
         let _result = Galois::new(BigUint::from(17u64), BigUint::ZERO);
+    }
+
+    #[test] // Galois::new(0, 13) == Galois::new(0, 13)
+    fn eq_check_zero() {
+        assert_eq!(
+            Galois::new(BigUint::from(0u64), BigUint::from(13u64)),
+            Galois::new(BigUint::from(0u64), BigUint::from(13u64))
+        );
+    }
+
+    #[test] // Galois::new(3, 17)  == Galois::new(3, 17)
+    fn eq_check_basic() {
+        assert_eq!(
+            Galois::new(BigUint::from(3u64), BigUint::from(17u64)),
+            Galois::new(BigUint::from(3u64), BigUint::from(17u64))
+        );
+    }
+
+    #[test] // Galois::new(20, 19) == Galois::new(1, 19)
+    fn eq_check_modulus() {
+        assert_eq!(
+            Galois::new(BigUint::from(20u64), BigUint::from(19u64)),
+            Galois::new(BigUint::from(1u64), BigUint::from(19u64))
+        );
+    }
+
+    #[test] // Galois::new(46, 23) == Galois::new(23, 23)
+    fn eq_check_multiple() {
+        assert_eq!(
+            Galois::new(BigUint::from(46u64), BigUint::from(23u64)),
+            Galois::new(BigUint::from(23u64), BigUint::from(23u64))
+        );
+    }
+
+    #[test] //
+    fn eq_check_big_prime() {
+        assert_eq!(
+            Galois::new(
+                "553059516537161321408265876840".parse::<BigUint>().unwrap(),
+                "553059516537161321408265876841".parse::<BigUint>().unwrap(),
+            ),
+            Galois::new(
+                "553059516537161321408265876840".parse::<BigUint>().unwrap(),
+                "553059516537161321408265876841".parse::<BigUint>().unwrap(),
+            )
+        );
+    }
+
+    #[test] // Galois::new(0, 29) != Galois::new(1, 29)
+    fn eq_check_unequal_num() {
+        assert_ne!(
+            Galois::new(BigUint::from(0u64), BigUint::from(29u64)),
+            Galois::new(BigUint::from(1u64), BigUint::from(29u64))
+        );
+    }
+
+    #[test] // Galois::new(0, 31) != Galois::new(0, 37)
+    fn eq_check_unequal_prime() {
+        assert_ne!(
+            Galois::new(BigUint::from(0u64), BigUint::from(31u64)),
+            Galois::new(BigUint::from(0u64), BigUint::from(37u64))
+        );
+    }
+
+    #[test] //
+    #[allow(non_snake_case)]
+    fn fmt_check() {
+        let one_in_F37 = Galois::new(BigUint::from(1u64), BigUint::from(37u64));
+        assert_eq!(format!("{one_in_F37:?}"), "Galois { num: 1, prime: 37 }");
+    }
+
+    #[test] // Galois::new(2, 41) == Galois::new(2, 41).clone()
+    #[allow(non_snake_case)]
+    fn clone_check() {
+        let two_in_F41 = Galois::new(BigUint::from(2u64), BigUint::from(41u64));
+        let two_in_F41_cloned = two_in_F41.clone();
+        assert_eq!(two_in_F41, two_in_F41_cloned);
     }
 }
